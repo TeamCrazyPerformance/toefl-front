f4e4809fcf172a30d9f3978249465c90c9f9a857
KangJi <kjhmd9726@gmail.com>
2020-03-17 19:00:16 +0900

    로그인이 필요한 페이지와 필요 없는 페이지 나눠서 라우팅

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,45 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+// Containers for public pages.
+import SignIn from "./containers/SignIn";
+import SignUp from "./containers/SignUp";
+// Containers for private pages.
+import Main from "./containers/Main";
+import MyInformation from "./containers/MyInformation";
+// Container for every pages.
+import NotFound from "./containers/NotFound";
 
-function Router() {
-  return <div>Router</div>;
-}
+const PublicRouter = () => (
+  <Switch>
+    <Route exact path="/" key="/" component={SignIn} />
+    <Route exact path="/signup" key="/signup" component={SignUp} />
+    <Route key="NotFound" component={NotFound} />
+  </Switch>
+);
 
-export default Router;
+const PrivateRouter = () => (
+  <Switch>
+    <Route exact path="/main" key="/main" component={Main} />
+    <Route
+      exact
+      path="/myinformation"
+      key="/myinformation"
+      component={MyInformation}
+    />
+    <Route key="NotFound" component={NotFound} />
+  </Switch>
+);
+
+const AppRouter = () => {
+  const isSignIn = false;
+
+  return <Router>{isSignIn ? <PrivateRouter /> : <PublicRouter />}</Router>;
+};
+
+export default AppRouter;
