3aef3bfc3ede2839b840ddd8313a4b8f448420a0
KangJi <kjhmd9726@gmail.com>
2020-03-18 19:10:12 +0900

    Private, Public router 컴포넌트 추가와 컨테이너 컴포넌트 index로 묶어서 export하도록 변경.

--- src/Router.js
+++ src/Router.js
@@ -1,45 +1,37 @@
 import React from "react";
-import PropTypes from "prop-types";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
-// Containers for public pages.
-import SignIn from "./containers/SignIn";
-import SignUp from "./containers/SignUp";
-// Containers for private pages.
-import Main from "./containers/Main";
-import MyInformation from "./containers/MyInformation";
-// Container for every pages.
-import NotFound from "./containers/NotFound";
-
-const PublicRouter = () => (
-  <Switch>
-    <Route exact path="/" key="/" component={SignIn} />
-    <Route exact path="/signup" key="/signup" component={SignUp} />
-    <Route key="NotFound" component={NotFound} />
-  </Switch>
-);
-
-const PrivateRouter = () => (
-  <Switch>
-    <Route exact path="/main" key="/main" component={Main} />
-    <Route
-      exact
-      path="/myinformation"
-      key="/myinformation"
-      component={MyInformation}
-    />
-    <Route key="NotFound" component={NotFound} />
-  </Switch>
-);
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { PublicRouter, PrivateRouter } from "./routerComponents";
+import { SignIn, SignUp, Main, MyInformation, NotFound } from "./containers";
 
 const AppRouter = () => {
-  const isSignIn = false;
-
-  return <Router>{isSignIn ? <PrivateRouter /> : <PublicRouter />}</Router>;
+  return (
+    <Router>
+      <Switch>
+        <PublicRouter
+          restricted={true}
+          component={SignIn}
+          path="/"
+          key="/"
+          exact
+        />
+        <PublicRouter
+          restricted={true}
+          component={SignUp}
+          path="/signup"
+          key="/signup"
+          exact
+        />
+        <PrivateRouter component={Main} path="/main" key="/main" exact />
+        <PrivateRouter
+          component={MyInformation}
+          path="/myinformation"
+          key="/myinformation"
+          exact
+        />
+        <PublicRouter restricted={false} component={NotFound} key="notfound" />
+      </Switch>
+    </Router>
+  );
 };
 
 export default AppRouter;
