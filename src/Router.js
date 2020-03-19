bce4355445e74b73400b0542b80baa6dce24597f
KangJi <kjhmd9726@gmail.com>
2020-03-20 00:59:02 +0900

    airbnb eslint config추가하고 Router 컴포넌트에는 다시 스타일 적용.

--- src/Router.js
+++ src/Router.js
@@ -3,35 +3,27 @@ import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { PublicRouter, PrivateRouter } from "./routerComponents";
 import { SignIn, SignUp, Main, MyInformation, NotFound } from "./containers";
 
-const AppRouter = () => {
-  return (
-    <Router>
-      <Switch>
-        <PublicRouter
-          restricted={true}
-          component={SignIn}
-          path="/"
-          key="/"
-          exact
-        />
-        <PublicRouter
-          restricted={true}
-          component={SignUp}
-          path="/signup"
-          key="/signup"
-          exact
-        />
-        <PrivateRouter component={Main} path="/main" key="/main" exact />
-        <PrivateRouter
-          component={MyInformation}
-          path="/myinformation"
-          key="/myinformation"
-          exact
-        />
-        <PublicRouter restricted={false} component={NotFound} key="notfound" />
-      </Switch>
-    </Router>
-  );
-};
+const AppRouter = () => (
+  <Router>
+    <Switch>
+      <PublicRouter restricted component={SignIn} path="/" key="/" exact />
+      <PublicRouter
+        restricted
+        component={SignUp}
+        path="/signup"
+        key="/signup"
+        exact
+      />
+      <PrivateRouter component={Main} path="/main" key="/main" exact />
+      <PrivateRouter
+        component={MyInformation}
+        path="/myinformation"
+        key="/myinformation"
+        exact
+      />
+      <PublicRouter restricted={false} component={NotFound} key="notfound" />
+    </Switch>
+  </Router>
+);
 
 export default AppRouter;
