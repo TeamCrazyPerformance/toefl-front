b18bd68b46ad9b64eb24dffd669043676266766f
KangJi <kjhmd9726@gmail.com>
2020-03-20 01:56:39 +0900

    jwt를 사용하면 사용하게 될 함수들 모음

--- src/helper/jwtHelper.js
+++ src/helper/jwtHelper.js
@@ -1,5 +1,29 @@
-const jwtHelper = () => {
-  console.log("This is jwtHelper");
+import jwtDecode from "jwt-decode";
+
+const jwtLocalStorageVariableName = "toeflJwt";
+
+export const getJwt = () => sessionStorage.getItem(jwtLocalStorageVariableName);
+
+export const setJwt = jwt => {
+  if (typeof jwt === "undefined") return false;
+  sessionStorage.setItem(jwtLocalStorageVariableName, jwt);
+  return true;
 };
 
-export default jwtHelper;
+export const removeJwt = () => {
+  sessionStorage.removeItem(jwtLocalStorageVariableName);
+  return true;
+};
+
+export const jwtExpiryValidate = () => {
+  const jwt = getJwt();
+
+  if (!jwt) return new Error("No jwt");
+
+  const decodedJwt = jwtDecode(jwt);
+  const currentTime = new Date().getTime();
+
+  // new Date().getTime() return millisecond(1/1000 second)
+  if (decodedJwt.exp > currentTime / 1000) return true;
+  return false;
+};
