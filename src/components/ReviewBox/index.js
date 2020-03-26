9a878983e9e9be4542e9dad566fafb23c4c5e45d
KangJi <kjhmd9726@gmail.com>
2020-03-26 22:43:20 +0900

    Sidebar 구조 추가
    sidebar 구현을 위한 구조 추가.

---
+++ src/components/ReviewBox/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReviewBox = () => {
+  return <div>ReviewtBox</div>;
+};
+
+export default ReviewBox;
--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Map from "../../components/Map";
+import Sidebar from "../../components/Sidebar";
 
 const Main = () => {
   const [center, setCenter] = useState({ lat: 37.6347813, lng: 127.0793528 });
