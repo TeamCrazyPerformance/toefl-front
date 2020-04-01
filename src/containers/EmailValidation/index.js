371905b33304c45394d1216586adb78a46df523c
KangJi <kjhmd9726@gmail.com>
2020-04-02 01:54:35 +0900

    SignUp page refactoring
    UI와 UX의 일을 모두다 presentational component에게 전달

--- src/containers/EmailValidation/index.js
+++ src/containers/EmailValidation/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import EmailValidationComponent from "../../components/EmailValidationComponent";
-import { useValidateInput } from "../../customHooks";
 import apiCallHelper from "../../helper/apiCallHelper";
 import * as authApi from "../../api/authApi";
 
@@ -10,105 +9,63 @@ const EmailValidation = props => {
     setEmailForRequestBody,
     setEmailValidation,
     setIsLoading,
-    setIsError,
-    cancelUrl
+    setIsError
   } = props;
 
-  const email = useValidateInput("");
-  const validationCode = useValidateInput("");
-
-  const validateEmail = (newValue = email.value) => {
-    return email.validate([
-      {
-        validation: !(newValue === ""),
-        validationFalse: "이메일을 입력해주세요"
+  const sendEmail = ({
+    setPendingFeedBackMsg,
+    setSuccessFeedBackMsg,
+    setFailFeedBackMsg,
+    email
+  }) => {
+    setPendingFeedBackMsg();
+    apiCallHelper(authApi.validateEmailFetcher({ email }), {
+      apiCallStart: () => setIsLoading(true),
+      apiCallSuccess: res => {
+        if (res.success) {
+          setIsLoading(false);
+          setEmailForRequestBody(email);
+          setSuccessFeedBackMsg();
+        } else {
+          setFailFeedBackMsg();
+        }
       },
+      apiCallFailure: () => setIsError(true)
+    });
+  };
+
+  const submitValidationCode = ({
+    setPendingFeedBackMsg,
+    setFailFeedBackMsg,
+    email,
+    validationCode
+  }) => {
+    setPendingFeedBackMsg();
+    apiCallHelper(
+      authApi.validateValidationCodeFetcher({
+        email: email.value,
+        validationCode: validationCode.value
+      }),
       {
-        validation: /^[A-Za-z0-9+]*(@seoultech.ac.kr)$/.test(newValue),
-        validationFalse: "서울과학기술대학교 이메일을 입력해주세요"
-      }
-    ]);
-  };
-
-  const updateAndValidateEmail = event => {
-    email.setValue(event.target.value);
-    validateEmail(event.target.value);
-  };
-
-  const validateValidationCode = (newValue = validationCode.value) => {
-    return validationCode.validate([
-      {
-        validation: !(newValue === ""),
-        validationFalse: "인증번호를 입력해주세요"
-      }
-    ]);
-  };
-
-  const updateAndValidateValidationCode = event => {
-    validationCode.setValue(event.target.value);
-    validateValidationCode(event.target.value);
-  };
-
-  const validateAndSendEmail = () => {
-    const emailValValidation = validateEmail();
-    if (emailValValidation) {
-      email.setFeedbackMsgAndValidation("잠시만 기다려주세요", true);
-      apiCallHelper(authApi.validateEmailFetcher({ email: email.value }), {
         apiCallStart: () => setIsLoading(true),
         apiCallSuccess: res => {
           if (res.success) {
             setIsLoading(false);
-            email.setFeedbackMsgAndValidation("이메일을 확인해주세요", true);
+            setEmailForRequestBody(email.value);
+            setEmailValidation(true);
           } else {
-            email.setFeedbackMsgAndValidation("이미 사용중인 이메일 입니다");
+            setFailFeedBackMsg();
           }
         },
         apiCallFailure: () => setIsError(true)
-      });
-    }
-  };
-
-  const validateAndSubmitValidationCode = () => {
-    const validationCodeValValidation = validateValidationCode();
-    if (validationCodeValValidation) {
-      validationCode.setFeedbackMsgAndValidation("잠시만 기다려주세요", true);
-      apiCallHelper(
-        authApi.validateValidationCodeFetcher({
-          email: email.value,
-          validationCode: validationCode.value
-        }),
-        {
-          apiCallStart: () => setIsLoading(true),
-          apiCallSuccess: res => {
-            if (res.success) {
-              setIsLoading(false);
-              setEmailForRequestBody(email.value);
-              setEmailValidation(true);
-            } else {
-              validationCode.setFeedbackMsgAndValidation(
-                "코드가 올바르지 않습니다"
-              );
-            }
-          },
-          apiCallFailure: () => setIsError(true)
-        }
-      );
-    }
+      }
+    );
   };
 
   return (
     <EmailValidationComponent
-      email={email.value}
-      emailValidation={email.validation}
-      emailFeedbackMsg={email.feedbackMsg}
-      updateAndValidateEmail={updateAndValidateEmail}
-      validateAndSendEmail={validateAndSendEmail}
-      validationCode={validationCode.value}
-      validationCodeValidation={validationCode.validation}
-      validationCodeFeedbackMsg={validationCode.feedbackMsg}
-      updateAndValidateValidationCode={updateAndValidateValidationCode}
-      validateAndSubmitValidationCode={validateAndSubmitValidationCode}
-      cancelUrl={cancelUrl}
+      sendEmail={sendEmail}
+      submitValidationCode={submitValidationCode}
     />
   );
 };
@@ -117,8 +74,7 @@ EmailValidation.propTypes = {
   setEmailForRequestBody: PropTypes.func.isRequired,
   setEmailValidation: PropTypes.func.isRequired,
   setIsLoading: PropTypes.func.isRequired,
-  setIsError: PropTypes.func.isRequired,
-  cancelUrl: PropTypes.string.isRequired
+  setIsError: PropTypes.func.isRequired
 };
 
 export default EmailValidation;
--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
-import PropTypes, { shape } from "prop-types";
 import LoadingSpinner from "../../components/LoadingSpinner";
 import PageError from "../PageError";
-
 import EmailValidation from "../EmailValidation";
 import AdditionalInformation from "../AdditionalInformation";
 
-const SignUp = props => {
-  const { history } = props;
+const SignUp = () => {
   const [isLoaing, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [emailForRequestBody, setEmailForRequestBody] = useState("");
@@ -25,27 +22,18 @@ const SignUp = props => {
               setEmailValidation={setEmailValidation}
               setIsLoading={setIsLoading}
               setIsError={setIsError}
-              cancelUrl={process.env.REACT_APP_MAIN_URL}
             />
           ) : (
             <AdditionalInformation
               emailForRequestBody={emailForRequestBody}
               setIsLoading={setIsLoading}
               setIsError={setIsError}
-              cancelUrl={process.env.REACT_APP_MAIN_URL}
-              moveToMain={() => history.push(process.env.REACT_APP_MAIN_URL)}
             />
           )}
         </>
       )}
     </LoadingSpinner>
   );
-};
-
-SignUp.propTypes = {
-  history: shape({
-    push: PropTypes.func.isRequired
-  }).isRequired
 };
 
 export default SignUp;
