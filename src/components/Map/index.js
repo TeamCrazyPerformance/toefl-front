e32d64a07f5b19aaf4f1390b5c7992465d72f56d
KangJi <kjhmd9726@gmail.com>
2020-04-05 00:19:32 +0900

    Map 컴포넌트에서의 hover 기능 추가
    main 컴포넌트에서 places 를 props로 전달 후 map 컴포넌트에서 places의 좌표를 place pin으로 보여주고, hover될 떄 place pin의 색을 변경한다.

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,30 +1,114 @@
-import React from "react";
-import PropTypes, { shape } from "prop-types";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import GoogleMapReact from "google-map-react";
+import PlacePin from "../PlacePin";
 
 const Map = props => {
-  const { center, zoom } = props;
+  const {
+    setCenter,
+    setZoom,
+    places,
+    setFocusedPlaceId,
+    defaultCenter,
+    defaultZoom
+  } = props;
+
+  const [hover, setHover] = useState(false);
+  const [hoveredPlace, setHoveredPlace] = useState({
+    id: "",
+    text: "",
+    lat: 0,
+    lng: 0
+  });
+
+  const onBoundsChange = (newcenter, newZoom) => {
+    setCenter(newcenter);
+    setZoom(newZoom);
+  };
+
+  const onChildClick = key => {
+    setFocusedPlaceId(key);
+  };
+
+  const onChildMouseEnter = (key, childProps) => {
+    setHover(true);
+    setHoveredPlace(childProps);
+  };
+
+  const onChildMouseLeave = () => {
+    setHover(false);
+    setHoveredPlace({
+      id: "",
+      key: "",
+      text: "",
+      lat: 0,
+      lng: 0
+    });
+  };
+
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
-        defaultCenter={center}
-        defaultZoom={zoom}
+        defaultCenter={defaultCenter}
+        defaultZoom={defaultZoom}
+        hoverDistance={20}
+        onBoundsChange={onBoundsChange}
+        onChildClick={onChildClick}
+        onChildMouseEnter={onChildMouseEnter}
+        onChildMouseLeave={onChildMouseLeave}
       >
-        <div lat={37.6347813} lng={127.0793528}>
-          여기는 학교!!!
-        </div>
+        {places.map(place => (
+          <PlacePin key={place.id} lat={place.lat} lng={place.lng} />
+        ))}
+        {hover ? (
+          <PlacePin
+            key={hoveredPlace.id}
+            lat={hoveredPlace.lat}
+            lng={hoveredPlace.lng}
+            color="blue"
+          />
+        ) : (
+          <></>
+        )}
       </GoogleMapReact>
     </div>
   );
 };
 
 Map.propTypes = {
-  center: shape({
+  setCenter: PropTypes.func.isRequired,
+  setZoom: PropTypes.func.isRequired,
+  places: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired
+    })
+  ),
+  setFocusedPlaceId: PropTypes.func.isRequired,
+  defaultCenter: PropTypes.shape({
     lat: PropTypes.number.isRequired,
     lng: PropTypes.number.isRequired
-  }).isRequired,
-  zoom: PropTypes.number.isRequired
+  }),
+  defaultZoom: PropTypes.number
+};
+
+Map.defaultProps = {
+  defaultCenter: {
+    lat: 37.6347813,
+    lng: 127.0793528
+  },
+  places: [
+    {
+      id: "",
+      text: "",
+      lat: 37.6347813,
+      lng: 127.0793528
+    }
+  ],
+  defaultZoom: 14
 };
 
 export default Map;
--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,14 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import Map from "../../components/Map";
 import Sidebar from "../../components/Sidebar";
 
 const Main = () => {
-  const [center, setCenter] = useState({ lat: 37.6347813, lng: 127.0793528 });
+  const [center, setCenter] = useState({
+    lat: 37.6347813,
+    lng: 127.0793528
+  });
   const [zoom, setZoom] = useState(14);
+  const [places, setPlaces] = useState([
+    {
+      id: "",
+      text: "",
+      lat: 0,
+      lng: 0
+    }
+  ]);
+  const [focusedPlaceId, setFocusedPlaceId] = useState("");
+
+  useMemo(() => {
+    setPlaces([
+      {
+        id: "A",
+        text: "A",
+        lat: 37.6357224,
+        lng: 127.0793213
+      },
+      {
+        id: "B",
+        text: "B",
+        lat: 37.6467314,
+        lng: 127.0793342
+      },
+      {
+        id: "C",
+        text: "C",
+        lat: 37.6577121,
+        lng: 127.0793132
+      },
+      {
+        id: "D",
+        text: "D",
+        lat: 37.66878414,
+        lng: 127.0793443
+      }
+    ]);
+  }, [center, zoom]);
 
   return (
     <div>
-      <Map center={center} zoom={zoom} />
+      <Map
+        setCenter={setCenter}
+        setZoom={setZoom}
+        places={places}
+        setFocusedPlaceId={setFocusedPlaceId}
+        defaultCenter={{
+          lat: 37.6347813,
+          lng: 127.0793528
+        }}
+        defaultZoom={14}
+      />
       {/* <Sidebar /> */}
     </div>
   );
