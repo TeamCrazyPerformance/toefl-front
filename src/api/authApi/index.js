703f27b760bb8fda13353b15a85f25395de5fe00
KangJi <kjhmd9726@gmail.com>
2020-04-09 02:24:07 +0900

    const에 환경변수 할당

--- src/api/authApi/index.js
+++ src/api/authApi/index.js
@@ -1,8 +1,10 @@
 import fetchHelper from "../../helper/fetchHelper";
 
+const serverUrl = process.env.REACT_APP_TOEFL_SERVER_URL;
+
 export const fetchSignIn = ({ id, password }) => {
   return fetchHelper({
-    url: `${process.env.REACT_APP_TOEFL_SERVER_URL}/login`,
+    url: `${serverUrl}/login`,
     method: "post",
     body: { id, password }
   }).then(responseJson => responseJson);
@@ -10,7 +12,7 @@ export const fetchSignIn = ({ id, password }) => {
 
 export const fetchValidateEmail = ({ email }) => {
   return fetchHelper({
-    url: `${process.env.REACT_APP_TOEFL_SERVER_URL}/user/email`,
+    url: `${serverUrl}/user/email`,
     method: "post",
     body: { email }
   }).then(responseJson => responseJson);
@@ -18,7 +20,7 @@ export const fetchValidateEmail = ({ email }) => {
 
 export const fetchValidateValidationCode = ({ email, validationCode }) => {
   return fetchHelper({
-    url: `${process.env.REACT_APP_TOEFL_SERVER_URL}/user/email/validation`,
+    url: `${serverUrl}/user/email/validation`,
     method: "post",
     body: { email, validationCode }
   }).then(responseJson => responseJson);
@@ -26,7 +28,7 @@ export const fetchValidateValidationCode = ({ email, validationCode }) => {
 
 export const fetchSignUp = ({ id, email, nickName, password }) => {
   return fetchHelper({
-    url: `${process.env.REACT_APP_TOEFL_SERVER_URL}/user`,
+    url: `${serverUrl}/user`,
     method: "post",
     body: {
       id,
