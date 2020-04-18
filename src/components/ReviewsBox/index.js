9bd02e33f6d9fdafade1603a75409e627bb8c93b
KangJi <kjhmd9726@gmail.com>
2020-04-19 02:20:54 +0900

    Reviews box 컴포넌트 추가
    추후 pagenation 기능 추가 가능성 때문에 하나의 층을 더 만듦

---
+++ src/components/ReviewsBox/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReviewBox from "../ReviewBox";
+
+const ReviewsBox = props => {
+  const { reviews } = props;
+
+  return (
+    <div>
+      {reviews.length ? (
+        reviews.map(review => {
+          return <ReviewBox key={review.reviewerId} />;
+        })
+      ) : (
+        <div>아직 리뷰가 없습니다.</div>
+      )}
+    </div>
+  );
+};
+
+ReviewsBox.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      reviewerId: PropTypes.string.isRequired,
+      score: PropTypes.number.isRequired,
+      content: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
+
+export default ReviewsBox;
