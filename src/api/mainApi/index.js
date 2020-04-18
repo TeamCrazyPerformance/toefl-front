d71aa941e22b99a47983c9e216575f9c0334ad2c
KangJi <kjhmd9726@gmail.com>
2020-04-19 00:02:37 +0900

    review api 추가

--- src/api/mainApi/index.js
+++ src/api/mainApi/index.js
@@ -33,3 +33,42 @@ export const fetchPlaceNearBy = (mapInstance, searchRadius) => {
     return newFormatPlaces;
   });
 };
+
+export const fetchPlaceStar = placeId => {
+  return fetchHelper({
+    url: `${serverUrl}/place/${placeId}/star`,
+    method: "get"
+  }).then(responseJson => responseJson);
+};
+
+export const fetchPlaceReview = (
+  placeId,
+  page = 0,
+  sort = "date",
+  order = "DESC"
+) => {
+  return fetchHelper({
+    url: `${serverUrl}/review?placeId=${placeId}&page=${page}&sort=${sort}&order=${order}`,
+    method: "get"
+  }).then(responseJson => responseJson);
+};
+
+export const fetchPostReview = (placeId, userId, score, content) => {
+  return fetchHelper({
+    url: `${serverUrl}/review`,
+    method: "post",
+    body: {
+      placeId,
+      userId,
+      score,
+      content
+    }
+  }).then(responseJson => responseJson);
+};
+
+export const fetchDeleteReview = reviewId => {
+  return fetchHelper({
+    url: `${serverUrl}/review/${reviewId}`,
+    method: "delete"
+  }).then(responseJson => responseJson);
+};
