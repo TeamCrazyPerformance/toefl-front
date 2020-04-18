96ee85a34145b357476df792641420154ffaf157
KangJi <kjhmd9726@gmail.com>
2020-04-18 23:09:14 +0900

    Reviewbox 컴포넌트 스타일 수정
    높은 재활용성을 위해서 width를 100%로 변경

--- src/components/ReviewBox/index.js
+++ src/components/ReviewBox/index.js
@@ -3,24 +3,19 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const ReviewBoxStyles = makeStyles(() => ({
   reviewBoxWrapper: {
-    display: "flex",
-    width: "20rem",
+    width: "100%",
     paddingBottom: "15px"
   },
   reviewBoxReviewerId: {
-    width: "20rem",
+    width: "100%",
     paddingBottom: "5px"
   },
   reviewBoxReviewerStar: {
-    width: "300px",
-    height: "1.3rem",
+    width: "100%",
     paddingBottom: "5px"
   },
   reviewBoxReviewContent: {
-    width: "20rem",
-    whiteSpace: "nowrap",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
+    width: "100%",
     paddingBottom: "5px"
   }
 }));
@@ -34,19 +29,17 @@ const ReviewBox = () => {
   } = ReviewBoxStyles();
 
   return (
-    <>
-      <div className={reviewBoxWrapper}>
-        <div className={reviewBoxReviewerId}>Place name</div>
-        <div className={reviewBoxReviewerStar}>star</div>
-        <div className={reviewBoxReviewContent}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </div>
+    <div className={reviewBoxWrapper}>
+      <div className={reviewBoxReviewerId}>Reviewer id</div>
+      <div className={reviewBoxReviewerStar}>star</div>
+      <div className={reviewBoxReviewContent}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua.
       </div>
-    </>
+    </div>
   );
 };
 
