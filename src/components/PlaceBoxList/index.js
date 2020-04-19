1955a2ec0b7c3874bf67229cf0a986b417a2b67f
KangJi <kjhmd9726@gmail.com>
2020-04-19 17:04:44 +0900

    Placebox list 컴포넌트 구현

---
+++ src/components/PlaceBoxList/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import PlaceBox from "../PlaceBox";
+
+const PlaceBoxList = props => {
+  const { places, hoveredPlaceId, setFocusedPlaceId } = props;
+
+  return (
+    <div>
+      {places.length ? (
+        places.map(place => (
+          <PlaceBox
+            place={place}
+            hoveredPlaceId={hoveredPlaceId}
+            setFocusedPlaceId={setFocusedPlaceId}
+            key={place.placeId}
+          />
+        ))
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+PlaceBoxList.propTypes = {
+  places: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      placeId: PropTypes.string.isRequired,
+      location: PropTypes.shape({
+        lat: PropTypes.number.isRequired,
+        lng: PropTypes.number.isRequired
+      }).isRequired
+    })
+  ).isRequired,
+  hoveredPlaceId: PropTypes.string.isRequired,
+  setFocusedPlaceId: PropTypes.func.isRequired
+};
+
+export default PlaceBoxList;
