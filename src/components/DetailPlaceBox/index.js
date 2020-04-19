6380b44a5446987514832adeec590afc15bc039e
KangJi <kjhmd9726@gmail.com>
2020-04-19 14:55:38 +0900

    place box 스타일 변경
    google api로 가져올 수 없는 정보 때문에 스타일 변경

--- src/components/DetailPlaceBox/index.js
+++ src/components/DetailPlaceBox/index.js
@@ -3,7 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const DetailPlaceBoxStyles = makeStyles(() => ({
   placeInfoBoxWrapper: {
-    width: "100%"
+    width: "100%",
+    marginBottom: "15px",
+    border: "1px solid black"
   },
   placeName: {
     width: "100%",
@@ -31,26 +33,15 @@ const DetailPlaceBoxStyles = makeStyles(() => ({
     width: "calc(100%-35px)",
     height: "1.3rem"
   },
-  placeLocationWapper: {
-    display: "flex",
-    paddingBottom: "5px"
-  },
-  placeInfoWrapper: {
-    width: "100%"
-  },
   placeLocationPhoneNum: {
-    width: "105px",
+    width: "100%",
     paddingRight: "10px"
   },
   placeLocationAddress: {
-    width: "calc(100%-105px)",
+    width: "100%",
     whiteSpace: "nowrap",
     overflow: "hidden",
     textOverflow: "ellipsis"
-  },
-  placeIntroduction: {
-    whiteSpace: "normal",
-    minHeight: "100px"
   }
 }));
 
@@ -61,11 +52,8 @@ const DetailPlaceBox = () => {
     placeRatingWrapper,
     placeRating,
     placeRatingStar,
-    placeLocationWapper,
     placeLocationPhoneNum,
-    placeLocationAddress,
-    placeInfoWrapper,
-    placeIntroduction
+    placeLocationAddress
   } = DetailPlaceBoxStyles();
 
   return (
@@ -78,26 +66,9 @@ const DetailPlaceBox = () => {
           <div className={placeRating}>4.0</div>
           <div className={placeRatingStar}>star</div>
         </div>
-        <div className={placeInfoWrapper}>
-          <div className={placeLocationWapper}>
-            <div className={placeLocationPhoneNum}>0100000000000</div>
-            <div className={placeLocationAddress}>
-              locat lorem sit amet, consectetur adipiscing elit, sed
-            </div>
-          </div>
-          <div className={placeIntroduction}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. consectetur Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum
-          </div>
+        <div className={placeLocationPhoneNum}>0100000000000</div>
+        <div className={placeLocationAddress}>
+          locat lorem sit amet, consectetur adipiscing elit, sed
         </div>
       </div>
     </>
